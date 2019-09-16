7e438bbac48bc00818c920e050fd7389e1b9f0f2
Gourav Bhattacharjee <gourav_bhattacharjee@Cathaypacific.com>
2019-09-17 01:56:58 +0800

    Capstone Porject

---
+++ My First Notebook.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Data Science Experience
+
+# **Gourav Bhattacharjee**  
+# My current designation is Solution Analyst and I aspire to be a Data Scientist
+
+# *I am interested in data science because I am already involved in Data Loading and Analysis for past 8+ years and now I want to level up in my profession to be a Data Scientist to bring out the intelligence of the Data and able to help making strategic decisions*
+
+# ### Example of addition of two numbers
+
+# In[3]:
+
+
+1+1
+
+
+# This is to ~~learnt~~ learn basic usage of Jupyter notebook
+# 
+# 1. Character Styling  
+#  - Strike through
+#  - Bold
+#  - Tilt etc.
+#  
+# 2. Format Rendering
+#  - Markdown
+#  - Code etc.
+# 
+# 
+# 
+# 
+# > Thanks and Regards
+
+# In[4]:
+
+
+import pandas as pd
+
+
+# In[5]:
+
+
+import numpy as np
+
+
+# In[6]:
+
+
+print (" Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
